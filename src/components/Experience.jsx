685e94403cf0734f7e533d195d11cbae46432a78
AlesyaMaltseva <alesya.maltseva@gmail.com>
2026-03-13 17:14:17 +0300

    fonts

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,19 +1,21 @@
 import { useState, useRef } from 'react';
-import uuid from 'react-uuid';
+import experience from '../parts/experience.js';
+
 // import 'bootstrap/dist/css/bootstrap.css'
 
 function Experience() {
 
-const items1 = ['Это первый пункт списка', 'Второй пункт', 'Третий пункт', 'Пункт четвертый', 'Пятый пункт списка'];   
+const experienceList = experience.map((item,i)=>
+    <div key={i}>
+      <div>{item.company}</div>
+      <div>{item.year}</div>
+      <div>{item.position}</div>
+      <div>{item.text}</div>
+    </div>
+)   
 
   return  <>    
-    <ul className="list-group">
-        {items1.map((item) => (
-            <li key={item} className="list-group-item" onClick={() => console.log(item)}>{item}</li>
-         ))}  
-    </ul>
-
-    <div>В CSS3 были добавлены новые относительные единицы измерения, такие как vh, vw, vmin, vmax. Эти единицы вычисляются относительно размеров окна браузера.</div>              
+    {experienceList}       
     </>  
 }
 
