6c38f9f0eba2c863d40576c1164f16d7f2a0f97d
AlesyaMaltseva <alesya.maltseva@gmail.com>
2026-03-17 19:13:38 +0300

    topmobile

--- src/components/TopMobile.jsx
+++ src/components/TopMobile.jsx
@@ -31,16 +31,16 @@ useGSAP(()=>{
 gsap.to(arrow.current, 
   {display:'block',
     opacity: 1,
-    duration: 0.2,
+    duration: 0.5,    
     scrollTrigger: {
-      trigger: "#topMenu",
-      start: 0,
-      toggleActions: "play none none reset",
+      trigger: topMenuMobile.current,
+      start: 300,
+      toggleActions: "play none none reverse",
 }})
 
-const menu = gsap.timeline().pause();
+const menuPlay = gsap.timeline().pause();
 
-menu.to("#one", {
+menuPlay.to("#one", {
   duration:.4,
   rotation:135,
   transformOrigin: "50% 50%",
@@ -81,93 +81,79 @@ menu.to("#one", {
   ease:'elastic.out',
 },)
 
-const menuMobileItemsSpan =  document.querySelectorAll('.menuMobileItems span');
 
-const accordeon = gsap.timeline().pause();
+//const menuMobileItemsLinks = menuMobileItems.current.children;
+const menuMobileItemsLinks = document.querySelectorAll('.menuMobileItems a span');
 
-accordeon.to(menuMobileItems.current, {
-  duration:0.1,  
-  visibility:'visible',
-},'<')
-.to(burgerMobileMenu.current, {    
-    backgroundColor:'#b9b9b9',
-    duration:.2,
-},'<')
-
-.fromTo(menuMobileItemsSpan, {
-    xPercent:200,
-    opacity:0,    
-}, {
-    xPercent:0,
-    opacity:1,
-    stagger:0.05,
-    duration:.3,
-},'<');
-
-const menuLinks = document.querySelectorAll('.menuMobileItems a');
 let playMenu = true;
 
 function clickButton() {  
    if (playMenu) { 
-      menu.play();
-      accordeon.play();       
+      menuPlay.play();            
       gsap.to(divGray.current, {
           display:'block',
-          opacity:0.4,
+          opacity:0.6,
           duration:.6,
-      });               
-      playMenu = false; 
-      //console.log(playMenu)    
+      }); 
+      gsap.to(menuMobileItemsLinks, {
+            display:'block',
+            x:0,
+            stagger:0.09,
+            duration:0.6,
+            ease:'back.out',
+          });              
+      playMenu = false;        
     }
     else  {
-      menu.reverse();
-      accordeon.reverse(); 
+      menuPlay.reverse();       
       gsap.to(divGray.current, {          
           opacity:0,
           duration:.6,
           display:'none',
-      });     
-      playMenu = true; 
-      //console.log(playMenu)           
+      }); 
+       gsap.to(menuMobileItemsLinks, {
+            x:250,
+            stagger:-0.07,
+            duration:0.6,
+            ease:'expo', 
+            display:'none', 
+          });   
+      playMenu = true;                 
     }  
 }
 
 let event;
-
 function isMobileDevice() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 if (isMobileDevice()) {
-    event = 'touchstart';
-    console.log("Это мобильное устройство");
+    event = 'touchstart';    
 } else {
-    event = 'click';
-    console.log("Это не мобильное устройство");
+    event = 'click';    
 }
 
+const targets = [burgerContainer.current, divGray.current, menuMobileItemsLinks];
 
-burgerContainer.current.addEventListener(event, clickButton);
-divGray.current.addEventListener(event, clickButton);
-for (let menuLink of menuLinks) {
-  menuLink.addEventListener(event, clickButton);  
- }
-
-// clickButton(burgerContainer.current, event);
-// clickButton(divGray.current, event);
-// for (let menuLink of menuLinks) {
-//   clickButton(menuLink, event);
-//  }
+targets.forEach((target) => {
+  if(target.length!==undefined) {
+    for (let link of target) {
+        link.addEventListener(event, clickButton);        
+      }
+  } else {
+      target.addEventListener(event, clickButton);  
+  }
+})
 
 });
 
     return <>    
       <div id="topMenu" className="topMenuMobile" ref={topMenuMobile}>              
         <AnchorLink href={headers[0].url}  data-tooltip={headers[0].name} className="tooltip" id="">
-         <span>
+         <div className="arrowContainer">
             <svg ref={arrow} version="1.1" id="arrow" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" xmlSpace="preserve" viewBox="0 0 50 30" enableBackground="new 0 0 50 25"  fill="#ffffff">
               <path d="M48.962,21.065c0,0.59-0.26,1.173-0.757,1.568c-0.866,0.687-2.124,0.541-2.81-0.324L25,7.578L4.48,22.243 c-0.687,0.865-1.944,1.011-2.81,0.324s-1.01-1.944-0.324-2.811l22.087-16.64C23.813,2.639,24.39,2.36,25,2.36 s1.188,0.278,1.567,0.757l21.961,16.706C48.82,20.191,48.962,20.63,48.962,21.065z"/>
             </svg>
-         </span>
+         </div>
         </AnchorLink>  
 
         <div className="burgerMobileMenu" ref={burgerMobileMenu}>
